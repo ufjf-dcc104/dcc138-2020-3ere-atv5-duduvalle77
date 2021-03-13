3d0e694456361dc7e7956201789228a04643d0a2
Eduardo Vieira <duduvalle77@yahoo.com.br>
2021-03-13 11:06:42 -0300

    Cria e usa uma classe de Cena

---
+++ js/Cena.js
@@ -0,0 +1,11 @@
+class Cena {
+    constructor(canvas){
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
+    }
+    desenhar(){
+        this.fillStyle = "grey";
+        this.ctx.fillRect(0, 0, this.canvas.width, ths.canvas.height);
+    }
+
+}
--- js/Main.js
+++ js/Main.js
@@ -1,3 +1,5 @@
 console.log("Hello");
 const canvas = document.querySelector("canvas");
 console.log(canvas);
+const cena1 = new Cena(canvas);
+cena1.desenhar();
