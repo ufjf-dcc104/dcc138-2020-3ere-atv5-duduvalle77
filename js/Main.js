d41c4ec4831ac9a4ecb57593752d716df07ad399
Eduardo Vieira <duduvalle77@yahoo.com.br>
2021-03-13 17:12:31 -0300

    Adiciona teste de colisao de sprites

--- js/Main.js
+++ js/Main.js
@@ -5,8 +5,9 @@ const canvas = document.querySelector("canvas");
 const cena1 = new Cena(canvas);
 
 const pc = new Sprite({vx: 10});
+const en1 = new Sprite({x: 200, color:"red"});
 cena1.adicionar(pc);
-
+cena1.adicionar(en1);
 cena1.iniciar();
 
 document.addEventListener("keydown", (e) => {
