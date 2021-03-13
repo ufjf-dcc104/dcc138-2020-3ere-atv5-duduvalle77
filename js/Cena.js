f5c5dd7f5a29ac028bfaff2e721f3c0ba77b5cbb
Eduardo Vieira <duduvalle77@yahoo.com.br>
2021-03-13 11:09:57 -0300

    Correcao de erros

--- js/Cena.js
+++ js/Cena.js
@@ -1,4 +1,4 @@
-class Cena {
+export default class Cena {
     constructor(canvas){
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
--- js/Main.js
+++ js/Main.js
@@ -1,5 +1,5 @@
-console.log("Hello");
+import Cena from "./Cena.js";
+
 const canvas = document.querySelector("canvas");
-console.log(canvas);
 const cena1 = new Cena(canvas);
 cena1.desenhar();
