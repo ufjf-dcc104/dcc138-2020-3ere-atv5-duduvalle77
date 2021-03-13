1af978a27eba4197de76fd44bc407ef6a0e89959
Eduardo Vieira <duduvalle77@yahoo.com.br>
2021-03-13 17:22:49 -0300

    Adiciona remocao de sprites

--- js/Main.js
+++ js/Main.js
@@ -8,6 +8,8 @@ const pc = new Sprite({vx: 10});
 const en1 = new Sprite({x: 200, color:"red"});
 cena1.adicionar(pc);
 cena1.adicionar(en1);
+cena1.adicionar(new Sprite({y: 10, color:"blue"}));
+
 cena1.iniciar();
 
 document.addEventListener("keydown", (e) => {
