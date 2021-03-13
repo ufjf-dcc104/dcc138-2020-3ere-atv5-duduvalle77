a7f92e33a91a79e48cf52d872730c67501b45c38
Eduardo Vieira <duduvalle77@yahoo.com.br>
2021-03-13 12:29:37 -0300

    Adiciona Sprite com construtor com objeto de param

--- js/Cena.js
+++ js/Cena.js
@@ -4,8 +4,8 @@ export default class Cena {
         this.ctx = canvas.getContext("2d");
     }
     desenhar(){
-        this.fillStyle = "grey";
-        this.ctx.fillRect(0, 0, this.canvas.width, ths.canvas.height);
+        this.ctx.fillStyle = "grey";
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
     }
 
 }
--- js/Main.js
+++ js/Main.js
@@ -1,5 +1,10 @@
 import Cena from "./Cena.js";
+import Sprite from "./Sprite.js";
 
 const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 const cena1 = new Cena(canvas);
 cena1.desenhar();
+
+const pc = new Sprite();
+pc.desenhar(ctx);
