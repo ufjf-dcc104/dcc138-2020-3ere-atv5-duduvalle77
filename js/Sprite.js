d0e8832812d2f7e53e71277ef033744f6e2b477c
Eduardo Vieira <duduvalle77@yahoo.com.br>
2021-03-13 21:23:44 -0300

    Adiciona Restricoes da esquerda

--- js/Sprite.js
+++ js/Sprite.js
@@ -38,8 +38,12 @@ export default class Sprite {
         );
     }
     aplicaRestricoes(dt){
+        this.aplicaRestricoesDireita(dt);
+        this.aplicaRestricoesEsquerda(dt);
+    }
+    aplicaRestricoesDireita(dt){
         const SIZE = this.cena.mapa.SIZE;
-        if(this.vx>0){
+        if(this.vx > 0){
             const pmx = this.mx + 1;
             const pmy = this.my;
             if(this.cena.mapa.tiles[pmy][pmx] !=0){
@@ -52,6 +56,25 @@ export default class Sprite {
                 if(this.colidiuCom(tile)){
                     this.vx = 0;
                     this.x = tile.x - tile.w/2 - this.w/2 - 1;
+                }
+            }
+        }
+    }
+    aplicaRestricoesEsquerda(dt){
+        const SIZE = this.cena.mapa.SIZE;
+        if(this.vx < 0){
+            const pmx = this.mx - 1;
+            const pmy = this.my;
+            if(this.cena.mapa.tiles[pmy][pmx] !=0){
+                const tile = {
+                    x: pmx*SIZE+SIZE/2,
+                    y: pmy*SIZE+SIZE/2,
+                    w: SIZE,
+                    h: SIZE
+                };
+                if(this.colidiuCom(tile)){
+                    this.vx = 0;
+                    this.x = tile.x + tile.w/2 + this.w/2 + 1;
                 }
             }
         }
