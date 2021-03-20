0bec3cd6d16d2faa374d2bd9d558f70b13445304
Eduardo Vieira <duduvalle77@yahoo.com.br>
2021-03-20 19:30:14 -0300

    Retirando criacao de sprites a cada 4 segundos

--- js/Main.js
+++ js/Main.js
@@ -4,7 +4,9 @@ import Mapa from "./Mapa.js";
 import Mixer from "./Mixer.js";
 import Sprite from "./Sprite.js";
 import modeloMapa1 from "../maps/mapa1.js";
+import InputManager from "./InputManager.js";
 
+const input = new InputManager();
 const mixer = new Mixer(10);
 const assets = new AssetManager(mixer);
 
@@ -19,6 +21,13 @@ assets.carregaAudio("boom", "assets/boom.wav");
 const canvas = document.querySelector("canvas");
 canvas.width = 14*32;
 canvas.height = 10*32;
+
+/*input.configurarTeclado({
+    "ArrowLeft": "MOVE_ESQUERDA",
+    "ArrowLeft": "MOVE_ESQUERDA",
+
+});*/
+
 const cena1 = new Cena(canvas,assets);
 
 const mapa1 = new Mapa(10,14,32);
@@ -27,47 +36,6 @@ cena1.configuraMapa(mapa1);
 
 const pc = new Sprite({x: 50, vx: 10});
 cena1.adicionar(pc);
-
-window.setInterval(adicionaSprite, 4000);
-function adicionaSprite() 
-{
-  let y = Math.floor(Math.random() * (mapa1.LINHAS - 2)) + 1;
-  let x = Math.floor(Math.random() * (mapa1.COLUNAS - 2)) + 1;
-
-  if (mapa1.tiles[y][x] == 0) 
-  {
-    y = mapa1.SIZE * y + mapa1.SIZE / 2;
-    x = mapa1.SIZE * x + mapa1.SIZE / 2;
-    if (x > 416 || x < 32 || y > 288 || y < 32) 
-    {
-        adicionaSprite();
-    }
-    else
-    {
-        let randomSpeed = Math.floor(Math.random() * 4);
-        let vx1 = 0;
-        let vy1 = 0;
-        switch (randomSpeed) {
-            case 0:
-                vx1 = 10;
-                break;
-            case 1:
-                vx1 = -10;
-                break;
-            case 2:
-                vy1 = 10;
-                break;
-            default:
-                vy1 = -10;
-                break;
-        }
-
-        cena1.adicionar(new Sprite({ x: x, y: y, vx: vx1, vy: vy1, color: "blue" }));
-    }
-  } else {
-    adicionaSprite();
-  }
-}
 
 cena1.iniciar();
 
