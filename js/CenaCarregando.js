fe16c7b4ab413421ddc4f39ac6bb7d72968bf158
Eduardo Vieira <duduvalle77@yahoo.com.br>
2021-03-21 14:59:48 -0300

    Adicionando estrutura base da fase1

--- js/CenaCarregando.js
+++ js/CenaCarregando.js
@@ -20,7 +20,7 @@ export default class CenaCarregando extends Cena{
         if(this.assets.acabou()){
             if(this.game.input.comandos.get("PROXIMA_CENA"))
             {
-                this.game.selecionaCena("jogo");
+                this.game.selecionaCena("fase1");
                 return;
             }
         }
